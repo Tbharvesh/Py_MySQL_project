5a6311ef6f4e6cecde1b5853c2437f44db069e74
Tbharvesh <114326628+Tbharvesh@users.noreply.github.com>
2023-08-13 23:02:55 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,4 +1,8 @@
 import mysql.connector as sqltor
+import tkinter
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
 
 mycon=sqltor.connect(host='localhost',username='root',password='Tab04082003@',database='project1')
 if mycon.is_connected():
@@ -9,96 +13,169 @@ cur=mycon.cursor()
 
 choice=None
 ch='Y'
-while choice!=7 and ch=='Y':
-    print("---------MENU--------")
-    print("1.Insert A Customer Record")
-    print("2.Display Customer and Booking Details")
-    print("3.Search Customer")
-    print("4.Update customer details")
-    print("5.Delete A Record")
-    print("6.Exit")
-    choice=int(input("Enter your choice:"))
-    a='Y'
-    if choice==1:
-        while a=='Y':
-            mycon=sqltor.connect(host='localhost',username='root',password='Tab04082003@',database='project1')
+
+def insert_record():
+    def submit():
+        cid = int(cid_entry.get())
+        cname = cname_entry.get()
+        phn = phn_entry.get()
+        email = email_entry.get()
+        age = int(age_entry.get())
+        room_no = int(room_no_entry.get())
+        room_type = room_type_entry.get()
+        check_in = check_in_entry.get()
+        check_out = check_out_entry.get()
+        cost = cost_entry.get()
+
+        try:
+            mycon = sqltor.connect(host='localhost', username='root', password='Tab04082003@', database='project1')
             if mycon.is_connected():
-                print("successfully connected to the database")
+                print("Successfully connected to the database")
+                cur = mycon.cursor()
+                st = "INSERT INTO customer (cid, cname, phn, email, age) VALUES ({},'{}','{}','{}',{})".format(cid, cname, phn, email, age)
+                cur.execute(st)
+                mycon.commit()
+                cur.close()
+                mycon.close()
+                messagebox.showinfo("Success", "Record inserted successfully.")
             else:
                 print("Error connecting to MySQL Database")
-            cur=mycon.cursor()
-            cid=int(input("Enter cid : "))
-            cname=input("Enter Customer Name :")
-            phn=input("Enter Phone No. :")
-            email=input("Enter Email :")
-            age=int(input("Enter Age :"))
-            # Add booking details also
-            Room_No=int(input("Enter Room No. :"))
-            Room_Type=input("Enter Room Type :")
-            check_in=input("Enter Check-in Dtae :")
-            check_out=input("Enter check-out date :")
-            cost=input("Enter cost :")
-            print("Want to add more records:(Y/N) ")
-            a=input("Enter your choice : ")
-            st= "insert into customer(cid,cname,phn,email,age) values({},'{}','{}','{}',{})".format(cid,cname,phn,email,age)
-            # st2="insert intp booking-details(cid,Room_No,Room_Type,check_in,check_out,cost) values({},{},'{}','{}','{}',{})".format(cid,Room_No,Room_Type,check_in,check_out,cost)
-            cur.execute(st)
-            # cur.execute(st2)
-            mycon.commit()
-    if choice==2:
-        mycon=sqltor.connect(host='localhost',username='root',password='Tab04082003@',database='project1')
-        if mycon.is_connected():
-            print("successfully connected to the database")
-        else:
-            print("Error connecting to MySQL Database")
-        cur=mycon.cursor()
-        cur.execute("Select * from customer,booking_details where customer.cid=booking_details.cid")
-        data=cur.fetchall()
-        for row in data:
-            print(row)
-    if choice==3:
-        mycon=sqltor.connect(host='localhost',username='root',password='Tab04082003@',database='project1')
-        if mycon.is_connected():
-            print("successfully connected to the database")
-        else:
-            print("Error connecting to MySQL Database")
-        cur=mycon.cursor()
-        search=int(input("Enter cid for Customer to be searched :"))
-        st="select * from customer where cid={}".format(search)
-        cur.execute(st)
-        data=cur.fetchall()
-        print(data)
-        mycon.commit()      
-    if choice==4:
-        mycon=sqltor.connect(host='localhost',username='root',password='Tab04082003@',database='project1')
-        if mycon.is_connected():
-            print("successfully connected to the database")
-        else:
-            print("Error connecting to MySQL Database")
-        cur=mycon.cursor()
-        cid=int(input("Enter customer id to be updated :"))
-        
-        value=input("Enter value to be updated :")
-        updated=input("Enter updated value :")
-        # st="update customer set {value}={} where cid={}".format(updated,cid)
-        st = "UPDATE customer SET {} = {} WHERE cid = {}".format(value, updated, cid)
-        cur.execute(st)
-        data=cur.fetchall()
-        print(data)
-    if choice==5:
-        mycon=sqltor.connect(host='localhost',username='root',password='Tab04082003@',database='project1')
-        if mycon.is_connected():
-            print("successfully connected to the database")
-        else:
-            print("Error connecting to MySQL Database")
-        cur=mycon.cursor()
-        cid=int(input("Enter customer whose information needs to be deleted :"))
-        st="delete from customer where cid={c}".format(c=cid)
-        cur.execute(st)
-        cur.execute("select * from customer")
-        data=cur.fetchall()
-        for row in data:
-            print(row)
-    if choice==6:
-        print("Have A Nice Day Ahead")
-    ch=input("Want to conyinue(Y/N)? :")
+        except Exception as e:
+            print("Error:", e)
+            messagebox.showerror("Error", "An error occurred while inserting the record.")
+
+    # Tkinter GUI window
+    root = tk.Tk()
+    root.title("Insert Customer Record")
+    window_width = 400
+    window_height = 600
+    root.geometry(f"{window_width}x{window_height}")
+
+   
+    cid_label = tk.Label(root, text="CID:")
+    cid_label.pack()
+    cid_entry = tk.Entry(root)
+    cid_entry.pack()
+
+    cname_label = tk.Label(root, text="Customer Name:")
+    cname_label.pack()
+    cname_entry = tk.Entry(root)
+    cname_entry.pack()
+
+    phn_label = tk.Label(root, text="Phone No.:")
+    phn_label.pack()
+    phn_entry = tk.Entry(root)
+    phn_entry.pack()
+
+    email_label = tk.Label(root, text="Email:")
+    email_label.pack()
+    email_entry = tk.Entry(root)
+    email_entry.pack()
+
+    age_label = tk.Label(root, text="Age:")
+    age_label.pack()
+    age_entry = tk.Entry(root)
+    age_entry.pack()
+
+    room_no_label = tk.Label(root, text="Room No.:")
+    room_no_label.pack()
+    room_no_entry = tk.Entry(root)
+    room_no_entry.pack()
+
+    room_type_label = tk.Label(root, text="Room Type:")
+    room_type_label.pack()
+    room_type_entry = tk.Entry(root)
+    room_type_entry.pack()
+
+    check_in_label = tk.Label(root, text="Check-in Date:")
+    check_in_label.pack()
+    check_in_entry = tk.Entry(root)
+    check_in_entry.pack()
+
+    check_out_label = tk.Label(root, text="Check-out Date:")
+    check_out_label.pack()
+    check_out_entry = tk.Entry(root)
+    check_out_entry.pack()
+
+    cost_label = tk.Label(root, text="Cost:")
+    cost_label.pack()
+    cost_entry = tk.Entry(root)
+    cost_entry.pack()
+
+    submit_button = tk.Button(root, text="Submit", command=submit)
+    submit_button.pack()
+
+
+
+
+def display_records():
+    def fetch_records():
+        try:
+            mycon = sqltor.connect(host='localhost', username='root', password='Tab04082003@', database='project1')
+            if mycon.is_connected():
+                print("Successfully connected to the database")
+                cur = mycon.cursor()
+                cur.execute("SELECT * FROM customer, booking_details WHERE customer.cid = booking_details.cid")
+                data = cur.fetchall()
+                for row in data:
+                    tree.insert("", "end", values=row)
+                cur.close()
+                mycon.close()
+            else:
+                print("Error connecting to MySQL Database")
+        except Exception as e:
+            print("Error:", e)
+            messagebox.showerror("Error", "An error occurred while fetching records.")
+
+    # Tkinter GUI window
+    root = tk.Tk()
+    root.title("Display Customer and Booking Details")
+    
+
+    
+    tree = ttk.Treeview(root, columns=("cid", "cname", "phn", "email", "age", "Room_No", "Room_Type", "check_in", "check_out", "cost"), show="headings")
+    tree.heading("cid", text="CID")
+    tree.heading("cname", text="Customer Name")
+    tree.heading("phn", text="Phone No.")
+    tree.heading("email", text="Email")
+    tree.heading("age", text="Age")
+    tree.heading("Room_No", text="Room No.")
+    tree.heading("Room_Type", text="Room Type")
+    tree.heading("check_in", text="Check-in Date")
+    tree.heading("check_out", text="Check-out Date")
+    tree.heading("cost", text="Cost")
+    tree.pack()
+
+    fetch_button = tk.Button(root, text="Fetch Records", command=fetch_records)
+    fetch_button.pack()
+
+
+def update_records():
+    pass
+
+# main window
+root = tk.Tk()
+root.title("Hotel Management System")
+# heading_label = tkinter.Label(root, text="Hotel Management System", font=("Helvetica", 16, "bold"))
+# heading_label.pack(pady=20)
+# buttons
+insert_button = tk.Button(root, text="Insert Record", command=insert_record)
+display_button = tk.Button(root, text="Display Records", command=display_records)
+update_button = tk.Button(root,text="Update Records",command=update_records)
+exit_button = tk.Button(root, text="Exit", command=root.quit)
+
+empty_columns = 2
+
+
+insert_button.grid(row=3, column=empty_columns, padx=30, pady=50)
+display_button.grid(row=3, column=empty_columns + 1, padx=30, pady=50)
+exit_button.grid(row=3, column=empty_columns + 2, padx=30, pady=50)
+
+
+window_width = 400
+window_height = 100
+root.geometry(f"{window_width}x{window_height}")
+
+# Start the GUI
+root.mainloop()
+
